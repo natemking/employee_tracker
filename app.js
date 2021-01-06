ed1cda3daa8dc237062c1a648da8f498d02ebcc2
Nate M King <72567521+natemking@users.noreply.github.com>
2021-01-05 19:05:12 -0500

    Created action question module. Built init function that prompts user for the initial action questions. Created swtich statement to handle what to do when the user chooses an answer

--- app.js
+++ app.js
@@ -1,2 +1,54 @@
+//** Dependencies ***//
+//===================//
 const inquirer = require('inquirer');
 const mysql = require('mysql');
+const path = require('path');
+
+//*** Directories ***//
+//===================//
+const LIB_DIR = path.resolve(__dirname, './app/lib');
+
+
+//*** Modules ***//
+//===============//
+const action = require(`${LIB_DIR}/action`);
+
+// Initialization function
+const init = async () => {
+  try {
+    let data = await inquirer.prompt(action);
+
+    switch (data.action) {
+        case 'View All Employees':
+            
+            break;
+        case 'View All Employees by Dept.':
+            
+            break;
+        case 'View All Employees by MGR':
+            
+            break;
+        case 'Add Employee':
+            
+            break;   
+        case 'Remove Employee':
+            
+            break;
+        case 'Update Employee Role':
+            
+            break;
+        case 'Update Employee MGR':
+            
+            break;
+        default:
+            break;
+    }
+    
+      
+  }catch(err) {
+      console.log(err);
+  }
+}
+
+
+init();
---
+++ app/lib/action.js
@@ -0,0 +1,17 @@
+module.exports =  [
+    {
+        type: 'list',
+        name: 'action',
+        message: 'What do you want to do?',
+        choices: [
+            'View All Employees',
+            'View All Employees by Dept.',
+            'View All Employees by MGR',
+            'Add Employee',
+            'Remove Employee',
+            'Update Employee Role',
+            'Update Employee MGR',
+        ],
+    },
+]
+
